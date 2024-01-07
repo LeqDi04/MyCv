3ec2860fcea0add195e5c05f7c75f4e719cc7879
LeqDi04 <Leqdik4@gmail.com>
2024-01-08 06:00:18 +0700

    Your commit message

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import AVT from '/Image/avt.jpg';
 
 const Profile: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
-    <img src='{Image}'alt="Profile" className="w-32 h-32 rounded-full border-4 border-white mt-4" />
+      <img src={AVT} alt="Profile" className="w-32 h-32 rounded-full border-4 border-white mt-4 object-cover" />
       <h1 className="text-xl font-bold my-2">Lê Quốc Duy</h1>
       <p className="text-sm">Web Developer</p>
     </div>
